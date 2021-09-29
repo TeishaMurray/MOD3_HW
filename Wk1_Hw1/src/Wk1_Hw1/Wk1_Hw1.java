3367d223cb4f30ec293e76fc15cec839f895f999
TeishaMurray <teishamurray@gmail.com>
2021-09-29 16:55:50 -0400

    initial commit

---
+++ Wk1_Hw1/src/Wk1_Hw1/Wk1_Hw1.java
@@ -0,0 +1,68 @@
+package studentGrades;
+
+public class StudentGrades {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		//-----Question 1------
+//		int grade = 67;
+//		if(grade >= 90) {
+//			System.out.println("A");
+//		}
+//		else if(grade >= 80) {
+//			System.out.println("B");
+//		}
+//		else if(grade >=70) {
+//			System.out.println("C");
+//		}
+//		else if(grade >= 56) {
+//			System.out.println("D");
+//		}
+//		else {
+//			System.out.println("F");
+//		}
+//	}
+
+//			//-----Question 2-----
+//		int num = 1;
+//		switch(num) {
+//		case 1:
+//			System.out.println("Monday");
+//			break;
+//		case 2: 
+//			System.out.println("Tuesday");
+//			break;
+//		case 3:
+//			System.out.println("Wednesday");
+//			break;
+//		case 4:
+//			System.out.println("Thursday");
+//			break;
+//		case 5:
+//			System.out.println("Friday");
+//			break;
+//		case 6:
+//			System.out.println("Saturday");
+//			break;
+//		case 7:
+//			System.out.println("Sunday");
+//		}
+		
+//		//-----Question 3_____
+//		int num =88;
+//		if(num % 2 != 0) {
+//			System.out.println("Cool");
+//		}
+//		else if(num % 2 == 0 && num >=2 && num <=5) {
+//			System.out.println("Not Cool");
+//		}
+//		else if(num % 2 == 0 && num >=6 && num <=20) {
+//			System.out.println("Cool");
+//		}
+//		else {
+//			System.out.println("Not Cool");
+//		}
+
+		
+		}
+}
