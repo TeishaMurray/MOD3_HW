26d6ff041ca00c6b0103c5cc65a5d7de4c1b295e
TeishaMurray <teishamurray@gmail.com>
2021-10-06 16:40:30 -0400

    Wk2 Hw1

---
+++ MOD3_Wk2_Hw1/src/Computer/Computer.java
@@ -0,0 +1,30 @@
+package Computer;
+
+public class Computer {
+
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	private int year;
+	private int cost;
+	
+	
+	
+
+}
